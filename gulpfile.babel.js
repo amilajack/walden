ddba4af4bad2c8eda945141fdc03d9c0b284edf6
Amila Welihinda <amilajack@gmail.com>
2016-03-23 17:11:15 -0700

    Updated gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,48 +1,45 @@
 import gulp from 'gulp'
 import del from 'del'
-import babel from 'gulp-babel'
+import gulpLoadPlugins from 'gulp-load-plugins'
 
-const scripts = [
-  //
-];
-const distPath = './dist'
+const $ = gulpLoadPlugins()
 
-// Remove all compiled scripts
-gulp.task('clean', cb => del(['./dist'], { dot: true }))
+const styles = {
+  output: [
+    './assets/scss/**/**/*.scss'
+  ],
+  input: [
+    './assets/css'
+  ]
+}
+const scripts = {
+  input: [],
+  output: []
+}
 
-// Watch scripts
-gulp.task('watch', () => {
-  gulp.watch(scripts, ['default']);
+// Remove all compiled styles and scripts
+gulp.task('clean', cb => del([styles.output], { dot: true }))
+
+// Bower
+gulp.task('bower', () => {
+  return $.bower()
+})
+
+gulp.task('styles', () => {
+  gulp
+    .src(styles.input)
+    .pipe($.sass().on('error', $.sass.logError))
+    .pipe($.autoprefixer())
+    .pipe(gulp.dest(styles.output))
+})
+
+gulp.task('styles:watch', () => {
+  gulp.watch(styles.input, ['styles'])
 })
 
 // Default task
-gulp.task('default', ['clean'], () => {
-
-  gulp
-    .src(scripts)
-    .pipe(babel())
-    .pipe(gulp.dest(distPath));
+gulp.task('default', ['clean'], cb => {
+  runSequence('bower', cb);
 })
 
-//
-//
-// var gulp = require('gulp'),
-//     sass = require('gulp-sass'),
-//     autoprefixer = require('gulp-autoprefixer');
-//
-// var input = './assets/scss/**/**/*.scss',
-//     output = './assets/css';
-//
-// gulp.task('sass', function () {
-//   gulp
-//     .src(input)
-//     .pipe(sass().on('error', sass.logError))
-//     // .pipe(autoprefixer())
-//     .pipe(gulp.dest(output));
-// });
-//
-// gulp.task('sass:watch', function () {
-//   gulp.watch(input, ['sass']);
-// });
-//
-// gulp.task('default', ['sass', 'sass:watch']);
+gulp.task('dev', ['styles', 'styles:watch'])
